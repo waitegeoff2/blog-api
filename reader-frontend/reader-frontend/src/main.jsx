641554db0001bfb410e7f8b33c2b2558985c60f3
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-09-02 13:56:48 -0400

    Setting up React App Set up login/register forms first

--- reader-frontend/reader-frontend/src/main.jsx
+++ reader-frontend/reader-frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // children: [
+    //   { index: true, element: <Home /> },
+    //   { path: 'shop', element: <Shop /> },
+    // ],
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
