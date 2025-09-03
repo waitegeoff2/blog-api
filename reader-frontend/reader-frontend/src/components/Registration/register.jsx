1b5d7bb3f43e178b28c862b51b66874cf60f935f
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-09-03 09:56:22 -0400

    Setting up registration form To post request my API

--- reader-frontend/reader-frontend/src/components/Registration/register.jsx
+++ reader-frontend/reader-frontend/src/components/Registration/register.jsx
@@ -1,9 +1,79 @@
 import { useState } from "react";
 
 export default function RegistrationForm() {
-    //handle submit
-    
-    return (
+    //handle submit (make post request to api, update user)
+    const [name, setName] = useState('');
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errArray, setErrArray] = useState([]);
 
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        fetch("http://localhost:3000/signup", { 
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, username, password }), 
+        })
+        .then((response) => {
+            console.log(response)
+            return response.json();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    }
+
+    return (
+        <div className="registration-section">
+            {errArray.map((error) =>(
+                <span>`${error}`</span>
+            ))}
+            <h2>Register:</h2>
+            <form className="registration-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Full Name:</label>
+                <input 
+                    type="text"
+                    id='name' 
+                    name='name'
+                    placeholder="Full Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <label htmlFor="name">Username:</label>
+                <input 
+                    type="email"
+                    id='username' 
+                    name='username'
+                    placeholder="Your email (aaa@aaa.com)"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                />
+                <input 
+                    type="password"
+                    id='password' 
+                    name='password'
+                    placeholder="Passwords must match"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <input 
+                    type="password"
+                    id='confirmPassword' 
+                    name='confirmPassword'
+                    placeholder="Passwords must match"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+            </form>
+        </div>
     )
 }
