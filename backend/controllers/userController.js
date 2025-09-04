3331f14fb6d3f50040a8a089f9c5be960858d8c9
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-09-04 15:52:14 -0400

    Added route to find info on specific user. Decodes a JWT token to return data on user. Passes back to REact frontend to use the data there.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,6 +28,19 @@ const db = require('../db/queries')
 //         }),
 // ]
 
+
+//get user data from jwt. use jwt to find user id, search db for that user, return user to react
+async function getUserData(req, res, next) {
+    //get the user data from the decoded JWT which is attached to req.user
+    try {
+        console.log(req.user)
+        let userData = req.user;
+        res.status(200).json({ userData })
+    } catch(error) {
+        next(error);
+    }
+}
+
 async function createUser(req, res, next) {
     try {
         const user = req.body;
@@ -58,6 +71,7 @@ async function deleteUser(req, res, next) {
 }
 
 module.exports = {
+    getUserData,
     createUser,
     becomeAuth,
     deleteUser
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
 const userRouter = Router();
 const userController = require('../controllers/userController')
+const passport = require("passport");
+require('../config/passport');
+
+//this is to get user data using id from JWT token, thus should be protected
+userRouter.get('/userinfo', passport.authenticate('jwt', { session: false }), userController.getUserData)
 
 userRouter.post('/signup', userController.createUser)
 
