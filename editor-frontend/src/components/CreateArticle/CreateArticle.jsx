b9849a323ab56b9a4d568bbb733a5be06e0ed2f8
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-09-09 13:34:32 -0400

    Added Create page to add new blog Post Adds it to the db

--- editor-frontend/src/components/CreateArticle/CreateArticle.jsx
+++ editor-frontend/src/components/CreateArticle/CreateArticle.jsx
@@ -1,11 +1,15 @@
 import { useState, useRef } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useOutletContext } from "react-router";
 
 export default function CreateArticle() {
+    
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
     const navigate = useNavigate();
 
-
+    const { user } = useOutletContext();
+    console.log(user)
     
     async function handleSubmit(e) {
         //post the article, make a post request to 
@@ -19,17 +23,14 @@ export default function CreateArticle() {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}` // Replace 'yourJwtToken' with the actual token
         },
-        body: JSON.stringify({ title: title, author, content: content }), 
+        body: JSON.stringify({ title: title, user: user, content: content }), 
         })
         .then((response) => {
             return response.json();
         })
         .then((response) => {   
             console.log(response)
-            navigate('/');
-            //trigger a page rerender each time
-            // const newValue = reRender + 1
-            // setRerender(newValue)
+            // navigate('/');
         })
         .catch((err) => {
             console.log(err);
@@ -39,7 +40,34 @@ export default function CreateArticle() {
 
     return (
         <>
-   
+        <h2>Create New Blog Post:</h2>
+        <form onSubmit={handleSubmit}>
+            <input
+                    type="hidden"
+                    name="hiddenField"
+                    value={user}
+            />
+            <div>
+                <label htmlFor="title">Title:</label>
+                <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+                />
+            </div>
+            <div>
+                <label htmlFor="content">Content:</label>
+                <textarea
+                id="content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                required
+                ></textarea>
+            </div>
+            <button type="submit">Create Post</button>
+        </form>
         </>
     )
 }
